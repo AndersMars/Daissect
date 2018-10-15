99c49e5a7e619e230fab997d1557a076529ff874
Anders Mars <engvall@hotmail.com>
2018-10-15 23:16:06 +0200

    C# calls from javaScript. Links.

--- Daissect.Server/Controllers/RepositoryController.cs
+++ Daissect.Server/Controllers/RepositoryController.cs
@@ -14,10 +14,39 @@ namespace Daissect.Server.Controllers
         {
             var repositories = new List<Repository>()
             {
-                new Repository() { Address = "github.com/AndersMars/Daissect.git", Description = "A repository exploration thing.", Color = "blue" },
-                new Repository() { Address = "github.com/AndersMars/Mars.CognitiveServices.git", Description = "Platform and engine for cognitive services.", Color = "yellow"  },
-                new Repository() { Address = "gitlab.com/gitlab-examples/cross-project-java/java-frontend.git", Description= "Brief description", Color = "green" },
-                //new Repository() { Address = "gitlab/andersmars.com/thelab.git",  Color = "red" }
+                new Repository() { Address = "github.com/AndersMars/Daissect.git", Description = "A repository exploration thing.", Color = "blue", Code = @"using Daissect.Shared;
+using Microsoft.AspNetCore.Blazor;
+using System.Net.Http;
+
+namespace Daissect.Client.Services
+{
+    public class RepositoryService
+    {
+        private readonly HttpClient _http;
+
+        public RepositoryService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<Repository>> GetRepositories()
+        {
+            return await _http.GetJsonAsync<List<Repository>>
+                ($""/api/Repository/GetRepositories"");
+        }
+    }
+}" },
+                new Repository() { Address = "github.com/AndersMars/Mars.CognitiveServices.git", Description = "Cognitive services. AI related services.", Color = "yellow"  },
+                new Repository() { Address = "gitlab.com/AndersMars/Mars.Platform", Description= "Platform repository with service contracts.", Color = "green", Code = @"namespace Daissect.Shared
+{
+    public class Repository
+    {
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public string Code { get; set; }
+    }
+}" },
             };
 
             return repositories;
--- Daissect.Shared/Repository.cs
+++ Daissect.Shared/Repository.cs
@@ -1,36 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Daissect.Shared
+﻿namespace Daissect.Shared
 {
     public class Repository
     {
         public string Address { get; set; }
         public string Description { get; set; }
         public string Color { get; set; }
-        public string Code { get; set; } = @"using Daissect.Shared;
-using Microsoft.AspNetCore.Blazor;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Net.Http;
-
-namespace Daissect.Client.Services
-{
-    public class RepositoryService
-    {
-        private readonly HttpClient _http;
-
-        public RepositoryService(HttpClient http)
-        {
-            _http = http;
-        }
-
-        public async Task<List<Repository>> GetRepositories()
-        {
-            return await _http.GetJsonAsync<List<Repository>>($""/api/Repository/GetRepositories"");
-        }
-    }
-}";
+        public string Code { get; set; }
     }
 }
